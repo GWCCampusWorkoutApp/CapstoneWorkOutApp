58fa0367608064f11486f33ecbd9b3486429076e
GWC <gwc@macbook-pro-3.local>
2018-06-26 14:39:05 -0700

    test4

--- CapstoneWorkOutApp/MenuViewController.swift
+++ CapstoneWorkOutApp/MenuViewController.swift
@@ -8,18 +8,16 @@
 
 import UIKit
 
-class MenuViewController: UIViewController , UITableViewDelegate , UITableViewDataSource {
+class MenuViewController: UIViewController  {
     
-    let title_arr = ["HOME", "ABOUT US", "RATE US", "SHARE", "LOGOUT"]
+    
     
 
-    @IBOutlet weak var menu_tableView: UITableView!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        menu_tableView.delegate = self
-        menu_tableView.dataSource = self
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,16 +25,7 @@ class MenuViewController: UIViewController , UITableViewDelegate , UITableViewDa
         // Dispose of any resources that can be recreated.
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return title_arr.count
-    }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "menu_cell") as! MenuTableViewCell
-        cell.label_title.text = title_arr[indexPath.row]
-        return cell
-    }
-
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -44,6 +33,6 @@ class MenuViewController: UIViewController , UITableViewDelegate , UITableViewDa
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
+    
 
 }
