459ff603ab23147b22ee271e24137373fe865fe4
GWC <gwc@mbpmac-44.local>
2018-06-26 15:01:16 -0700

    bygvtxdeszswe

--- CapstoneWorkOutApp/SnacksViewController.swift
+++ CapstoneWorkOutApp/SnacksViewController.swift
@@ -30,9 +30,6 @@ class SnacksViewController: UIViewController {
         titleLabel.text = titleRec[randomIndex]
     }
     
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
