b100679472bc21bd188a4accc1f5c11eebb20526
GWC <gwc@macbook-pro-5.local>
2018-06-26 13:50:29 -0700

    the navigation / menu bar is FINISHED!!

--- CapstoneWorkOutApp/ViewController.swift
+++ CapstoneWorkOutApp/ViewController.swift
@@ -10,18 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+    @IBOutlet weak var menuView: UIView!
     @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
     var menuShowing = false
     override func viewDidLoad() {
         super.viewDidLoad()
+        menuView.layer.shadowOpacity = 5
+        menuView.layer.shadowRadius = 10
         
     }
 
     @IBAction func openMenu(_ sender: Any){
         if (menuShowing){
         leadingConstraint.constant = -264
+            UIView.animate(withDuration: 0.3, animations:{
+                self.view.layoutIfNeeded()
+            })
+    
         }else{
         leadingConstraint.constant = 0
+            UIView.animate(withDuration: 0.3, animations:{
+                self.view.layoutIfNeeded()
+                
+            })
+        
         }
         menuShowing = !menuShowing
     }
