1f4b4a81d15a0f21745f4c49423a206b365edc70
GWC <gwc@macbook-pro-5.local>
2018-06-26 11:59:20 -0700

    nav bar slides out

--- CapstoneWorkOutApp/ViewController.swift
+++ CapstoneWorkOutApp/ViewController.swift
@@ -10,16 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
+    var menuShowing = false
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func openMenu(_ sender: Any){
+        if (menuShowing){
+        leadingConstraint.constant = -264
+        }else{
+        leadingConstraint.constant = 0
+        }
+        menuShowing = !menuShowing
     }
-
-
 }
 
